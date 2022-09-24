a82031f7eb84167cdf5bf132b6d7f0f4a5b8e3ee
Shevchenko Yaroslav <shevkomore@gmail.com>
2022-09-24 18:30:13 +0300

    Transfigurator class, applies list to frame

--- transfigurator.py
+++ transfigurator.py
@@ -5,14 +5,19 @@ class Transfigurator:
         self.list_gen = list_generator
     
     def encode(self, frame_matrix):
-        perms = self.list_gen #TODO swap to true frame array
-        for i in range(0, perms.length+1, 4):
-            #TODO color encoding
-            frame_matrix[[perms[i], perms[i+1]],:] = frame_matrix[[perms[i+1], perms[i]],:]
-            frame_matrix[:,[perms[i+2], perms[i+3]]] = frame_matrix[:,[perms[i+3], perms[i+2]]]
+        perms = self.list_gen.generate()
+        for c in range(0,3): # RGB
+            for i in range(0, perms.length+1, 4):
+                #TODO color encoding
+                frame_matrix[perms[c][i][0], perms[c][i][1],:] = frame_matrix[perms[c][i][1], perms[c][i][0],:]
+                frame_matrix[:,perms[c][i][2], perms[c][i][3]] = frame_matrix[:,perms[c][i][3], perms[c][i][2]]
         return frame_matrix
 
     def decode(self, frame_matrix):
-        perms = self.list_gen
-        for i in range(perms.length, -1, -4):
-            #TODO color encoding
+        perms = self.list_gen.generate()
+        for c in range(0,3): # RGB
+            for i in range(perms.length, -1, -1):
+                #TODO color encoding
+                frame_matrix[:,perms[c][i][2], perms[c][i][3]] = frame_matrix[:,perms[c][i][3], perms[c][i][2]]
+                frame_matrix[perms[c][i][0], perms[c][i][1],:] = frame_matrix[perms[c][i][1], perms[c][i][0],:]
+        return frame_matrix
